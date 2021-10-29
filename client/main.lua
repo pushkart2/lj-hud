db20b38bf8fd7ee9df75cbe3573d6ecc8e289e0b
loljoshie <91661118+loljoshie@users.noreply.github.com>
2021-10-29 01:24:29 -0600

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -110,6 +110,9 @@ Citizen.CreateThread(function()
             
                 -- fuel
 		local fuel = exports['LegacyFuel']:GetFuel(GetVehiclePedIsIn(PlayerPedId(), false))
+				
+		-- uncomment and remove export above if using lj-fuel		
+		-- local fuel = exports['lj-fuel']:GetFuel(GetVehiclePedIsIn(PlayerPedId(), false))
 
                 -- engine
                 local engine = (GetVehicleEngineHealth(GetVehiclePedIsIn(PlayerPedId()))/10)
